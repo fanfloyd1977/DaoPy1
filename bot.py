331e0f0955e14744054a2983718f8b88cd6cf5a9
fanfloyd1977 <77666154+fanfloyd1977@users.noreply.github.com>
2021-01-23 16:48:21 +0700

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,7 +1,42 @@
-from flask import Flask
+import os
+from flask import Flask, jsonify, request, abort
+
 app = Flask(__name__)
+
+from linebot import (LineBotApi, WebhookHandler)
+from linebot.exceptions import (InvalidSignatureError)
+from linebot.models import (MessageEvent, TextMessage, TextSendMessage)
+
+
+
+line_bot_api = LineBotApi("1655584117")
+handler = WebhookHandler("4088552f2e9ee28de065d9bddce75ab2")
+
 @app.route("/")
+
 def hello():
-    return "Hello World!"
+    return "Hello DAO DAO DAo Flask-Heroku"
+
+@app.route("/webhook", methods=["GET","POST"])
+
+def webhook():
+    signature = request.headers["X-Line-Signature"]
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body:" + body)
+
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+    return "OK"
+
+@handler.add(MessageEvent, message=TextMessage)
+
+def handle_message(event):
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=event.message.text))
+
+
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
