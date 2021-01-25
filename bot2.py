918170ce0e3943682a5490f5b97ad29dcffd1a96
fanfloyd1977 <77666154+fanfloyd1977@users.noreply.github.com>
2021-01-25 15:42:41 +0700

    Add files via upload

--- bot2.py
+++ bot2.py
@@ -9,33 +9,16 @@ app = Flask(__name__)
 
 line_bot_api = LineBotApi("1655584117")
 handler = WebhookHandler("4088552f2e9ee28de065d9bddce75ab2")
-user_profile = "far"
-
-@app.route("/")
-
-def hello():
-    return "Hello DAO Flask-Heroku"
-
-@app.route("/", methods=['GET'])
-def home():
-    profile = line_bot_api.get_profile(user_profile)
-
-    print(profile.display_name)
-    print(profile.user_id)
-    print(profile.picture_url)
-    print(profile.status_message)
-    return '<div><h1>ok</h1></div>'
 
 @app.route("/callback", methods=["POST"])
 def callback():
     signature = request.headers["X-Line-Signature"]
     body = request.get_data(as_text=True)
-    app.logger.info("Request body:" + body)
+    app.logger.info("Request body: " + body)
 
     try:
         handler.handle(body, signature)
     except InvalidSignatureError:
-        print("Invalid signature. Please check your channel access token/channel secret.")
         abort(400)
     return "OK"
 
@@ -43,8 +26,9 @@ def callback():
 def handle_message(event):
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text="hello world"))
+        TextSendMessage(text=event.message.text))
 
 
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
