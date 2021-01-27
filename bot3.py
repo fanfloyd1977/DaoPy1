dee7f6b86128fe34dcca2ad7d9911d6087226ded
fanfloyd1977 <77666154+fanfloyd1977@users.noreply.github.com>
2021-01-27 14:30:02 +0700

    Add files via upload

--- bot3.py
+++ bot3.py
@@ -1,17 +1,63 @@
+import os
+from flask import Flask, request, abort
+
+from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError
+from linebot.models import MessageEvent, TextMessage, TextSendMessage
+
+
 from chatterbot import ChatBot
-from flask import Flask, render_template, request
+from chatterbot.trainers import ListTrainer
 
 app = Flask(__name__)
-app.static_folder = 'static'
+
+line_bot_api = LineBotApi("ziev+1/ECWJDjw1CkOPjOMofjQ5mft0H0XtZknC/Vu+KnGZzi+2vFVF34UiX+QOdh4JADi+j/xeyPeSiGjyhnvTvKjNijstiixgQeY77aBxJ7R0B8TS/BMCG/y8KheHMwAZ7TJFKN6i5UPBoRzm2BQdB04t89/1O/w1cDnyilFU=")
+handler = WebhookHandler("4088552f2e9ee28de065d9bddce75ab2")
+
+
+chatbot = ChatBot("Miss Lee")
+trainer = ListTrainer(chatbot)
+
+chatbot.storage.drop()
+
+
+trainer.train(["Hi there", "Hello"])
+trainer.train(["hi Dao", "I'm fine, thank you, and you?"])
+trainer.train(["Greetings", "Hello!"])
+trainer.train(["Who are you?", "I am Dan"])
+trainer.train(["สวัสดีค่ะ", "สวัสดีค่ะ สบายดีมั้ยคะ"])
+trainer.train(["ชื่ออะไรคะ", "ชื่อดาวค่ะ"])
+trainer.train(["I would like to know about CUBE-X", "CUBE-X is a product of Thailand created by Nanotec"])
+trainer.train(["tell me about CUBE-X", "Cubic hybrids mesoporous structure for antimicrobial on surfaces"])
+
+
+
 
 @app.route("/")
-def home():
-    return render_template("index.html")
 
-@app.route("/get")
-def get_bot_response():
-    userText = request.args.get('msg')
-    return str(ChatBot.get_response(userText))
+def hello():
+    return "Hello DAO Flask-Heroku"
+
+
+@app.route("/callback", methods=["POST"])
+def callback():
+    signature = request.headers["X-Line-Signature"]
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+    return "OK"
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=chatbot.get_response())
+        )
+
 
 if __name__ == "__main__":
     app.run()
