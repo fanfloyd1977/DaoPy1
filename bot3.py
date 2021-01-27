308d5c03fecd43278ad8c1b0d8abbc7bc2916da1
fanfloyd1977 <77666154+fanfloyd1977@users.noreply.github.com>
2021-01-27 14:55:49 +0700

    Update bot3.py

--- bot3.py
+++ bot3.py
@@ -14,11 +14,9 @@ app = Flask(__name__)
 line_bot_api = LineBotApi("ziev+1/ECWJDjw1CkOPjOMofjQ5mft0H0XtZknC/Vu+KnGZzi+2vFVF34UiX+QOdh4JADi+j/xeyPeSiGjyhnvTvKjNijstiixgQeY77aBxJ7R0B8TS/BMCG/y8KheHMwAZ7TJFKN6i5UPBoRzm2BQdB04t89/1O/w1cDnyilFU=")
 handler = WebhookHandler("4088552f2e9ee28de065d9bddce75ab2")
 
-
+"""
 chatbot = ChatBot("Miss Lee")
 trainer = ListTrainer(chatbot)
-
-chatbot.storage.drop()
 
 
 trainer.train(["Hi there", "Hello"])
@@ -30,7 +28,7 @@ trainer.train(["ชื่ออะไรคะ", "ชื่อดาวค่ะ
 trainer.train(["I would like to know about CUBE-X", "CUBE-X is a product of Thailand created by Nanotec"])
 trainer.train(["tell me about CUBE-X", "Cubic hybrids mesoporous structure for antimicrobial on surfaces"])
 
-
+"""
 
 
 @app.route("/")
