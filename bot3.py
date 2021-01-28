37022bf7de8b6bb62e9f71c56f321d1eb2ab3895
fanfloyd1977 <77666154+fanfloyd1977@users.noreply.github.com>
2021-01-28 17:36:06 +0700

    Add files via upload

--- bot3.py
+++ bot3.py
@@ -13,12 +13,19 @@ handler = WebhookHandler("4088552f2e9ee28de065d9bddce75ab2")
 
 greeting = ["Hello",
             "Hi, Dao :)", "Hi, There!", "Howdy", "สวัสดีครับ"]
-value1 = randrange(1, len(greeting))
+
 
 emo = ["How are you?", "how r u?",
        "Im fine, Thanks you and you", "I want to fly away", "Someday, I will fly", "I'm doing well", "I miss you"]
-#value2 = randrange(2, len(emo))
 
+address = ["Nanotec thai",
+            "ลัพธ์พร วยาจุต ศูนย์นาโนเทคโนโลยีแห่งชาติ (นาโนเทค) 111 อุทยานวิทยาศาสตร์ประเทศไทย ถนน พหลโยธิน ต.คลองหนึ่ง อ.คลองหลวง 12120",
+            "Nanotec eng",
+            "Dr. Lapporn Vayachuta National Science and Technology Development Agency 111 Thailand Science Park (TSP) Phahonyothin Road, Khlong Nueng, Khlong Luang, Pathum Thani 12120, Thailand",
+            "Home non thai",
+            "74/74 ชูชาติอนุสรณ์ 7 เลี่ยงเมืองปากเกร็ด 46 ตำบลบางตลาด อำเภอ ปากเกร็ด นนทบุรี 11120"
+            "Home non eng",
+            "74/74 Chuchat Anuson 7,Liang mueang pak kret 46,Bang Talat, Pak Kret District, Nonthaburi, 11120"]
 
 
 @app.route("/")
@@ -53,6 +60,7 @@ def handle_message(event):
             event.reply_token,
             TextSendMessage(text=reply_text))
         if event.message.text == greeting[0]:
+            value1 = randrange(1, len(greeting))
             reply_text = greeting[value1]
             line_bot_api.reply_message(
                 event.reply_token,
@@ -63,8 +71,16 @@ def handle_message(event):
             line_bot_api.reply_message(
                 event.reply_token,
                 TextSendMessage(text=reply_text))
-
-
+        if event.message.text == address[0]:
+            reply_text = address[1]
+            line_bot_api.reply_message(
+                event.reply_token,
+                TextSendMessage(text=reply_text))
+        if event.message.text == address[2]:
+            reply_text = address[3]
+            line_bot_api.reply_message(
+                event.reply_token,
+                TextSendMessage(text=reply_text))
 
 
 if __name__ == "__main__":
