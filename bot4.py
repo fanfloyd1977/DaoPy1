4201e13135a8b89eac7a02a22e5c49d24ed4b328
fanfloyd1977 <77666154+fanfloyd1977@users.noreply.github.com>
2021-01-31 18:38:59 +0700

    Add files via upload

--- bot4.py
+++ bot4.py
@@ -12,13 +12,13 @@ line_bot_api = LineBotApi("ziev+1/ECWJDjw1CkOPjOMofjQ5mft0H0XtZknC/Vu+KnGZzi+2vF
 handler = WebhookHandler("4088552f2e9ee28de065d9bddce75ab2")
 
 
-@app.route("/")
-def hello():
-    return "Hello DAO Flask-Heroku"
+#@app.route("/")
+#def hello():
+#    return "Hello DAO Flask-Heroku"
 
 
-@app.route("/callback", methods=["POST", "GET"])
-def callback():
+@app.route("/", methods=["POST", "GET"])
+def webhook():
     if request.method == "POST":
         payload = request.json
         Reply_token = payload["event"][0]["replyToken"]
@@ -29,6 +29,8 @@ def callback():
             Reply_message = "Good job"
             ReplyMessage(Reply_token,Reply_message,"ziev+1/ECWJDjw1CkOPjOMofjQ5mft0H0XtZknC/Vu+KnGZzi+2vFVF34UiX+QOdh4JADi+j/xeyPeSiGjyhnvTvKjNijstiixgQeY77aBxJ7R0B8TS/BMCG/y8KheHMwAZ7TJFKN6i5UPBoRzm2BQdB04t89/1O/w1cDnyilFU=")
         return request.json, 200
+    else:
+        abort(400)
 
 def ReplyMessage(Reply_token, TextMessage, Line_Acees_Token):
     LINE_API = "https://api.line.me/v2/bot/message/reply"
