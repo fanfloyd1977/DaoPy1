be76be0fff71a8cfa4433168cd1d370cf78f5aa5
fanfloyd1977 <77666154+fanfloyd1977@users.noreply.github.com>
2021-01-31 17:27:31 +0700

    Add files via upload

--- bot4.py
+++ bot4.py
@@ -1,45 +1,48 @@
-from flask import Flask, request
-import json
-import requests
+from chatterbot import ChatBot
+from chatterbot.trainers import ListTrainer
+
+from flask import Flask, request, abort
+
+from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError
+from linebot.models import MessageEvent, TextMessage, TextSendMessage
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return "Hello World!"
-# ส่วน callback สำหรับ Webhook
-@app.route('/callback', methods=['POST'])
+line_bot_api = LineBotApi("ziev+1/ECWJDjw1CkOPjOMofjQ5mft0H0XtZknC/Vu+KnGZzi+2vFVF34UiX+QOdh4JADi+j/xeyPeSiGjyhnvTvKjNijstiixgQeY77aBxJ7R0B8TS/BMCG/y8KheHMwAZ7TJFKN6i5UPBoRzm2BQdB04t89/1O/w1cDnyilFU=")
+handler = WebhookHandler("4088552f2e9ee28de065d9bddce75ab2")
+
+chatbot = ChatBot("Miss Lee")
+trainer = ListTrainer(chatbot)
+
+trainer.train(["I would like to know about CUBE-X", "CUBE-X is a product of Thailand created by Nanotec"])
+
+@app.route("/")
+def hello():
+    return "Hello DAO Flask-Heroku"
+
+
+@app.route("/callback", methods=["POST"])
 def callback():
-    json_line = request.get_json()
-    json_line = json.dumps(json_line)
-    decoded = json.loads(json_line)
-    user = decoded["events"][0]['replyToken']
-    #id=[d['replyToken'] for d in user][0]
-    #print(json_line)
-    print("ผู้ใช้：",user)
-    sendText(user,'งง') # ส่งข้อความ งง
-    return '',200
+    signature = request.headers["X-Line-Signature"]
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
 
-def sendText(user, text):
-    LINE_API = 'https://api.line.me/v2/bot/message/reply'
-    Authorization = 'ziev+1/ECWJDjw1CkOPjOMofjQ5mft0H0XtZknC/Vu+KnGZzi+2vFVF34UiX+QOdh4JADi+j/xeyPeSiGjyhnvTvKjNijstiixgQeY77aBxJ7R0B8TS/BMCG/y8KheHMwAZ7TJFKN6i5UPBoRzm2BQdB04t89/1O/w1cDnyilFU=' # ใส่ ENTER_ACCESS_TOKEN เข้าไป
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+    return "OK"
 
-    headers = {
-        'Content-Type': 'application/json; charset=UTF-8',
-        'Authorization':Authorization
-    }
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    value = chatbot.get_response(MessageEvent)
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=value)
+    )
 
-    data = json.dumps({
-        "replyToken":user,
-        "messages":[{
-            "type":"text",
-            "text":text
-        }]
-    })
 
-    #print("ข้อมูล：",data)
-    r = requests.post(LINE_API, headers=headers, data=data) # ส่งข้อมูล
-    #print(r.text)
+if __name__ == "__main__":
+    app.run()
 
-if __name__ == '__main__':
-    app.run(debug=True)
