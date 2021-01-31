407b75b9c50da2309f7b4cad5e6be25b5d5a4b2c
fanfloyd1977 <77666154+fanfloyd1977@users.noreply.github.com>
2021-01-31 18:13:44 +0700

    Add files via upload

--- bot4.py
+++ bot4.py
@@ -1,7 +1,6 @@
-from chatterbot import ChatBot
-from chatterbot.trainers import ListTrainer
-
 from flask import Flask, request, abort
+import requests
+import json
 
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
@@ -12,35 +11,43 @@ app = Flask(__name__)
 line_bot_api = LineBotApi("ziev+1/ECWJDjw1CkOPjOMofjQ5mft0H0XtZknC/Vu+KnGZzi+2vFVF34UiX+QOdh4JADi+j/xeyPeSiGjyhnvTvKjNijstiixgQeY77aBxJ7R0B8TS/BMCG/y8KheHMwAZ7TJFKN6i5UPBoRzm2BQdB04t89/1O/w1cDnyilFU=")
 handler = WebhookHandler("4088552f2e9ee28de065d9bddce75ab2")
 
-chatbot = ChatBot("Miss Lee")
-trainer = ListTrainer(chatbot)
-
-trainer.train(["I would like to know about CUBE-X", "CUBE-X is a product of Thailand created by Nanotec"])
 
 @app.route("/")
 def hello():
     return "Hello DAO Flask-Heroku"
 
 
-@app.route("/callback", methods=["POST"])
+@app.route("/callback", methods=["POST", "GET"])
 def callback():
-    signature = request.headers["X-Line-Signature"]
-    body = request.get_data(as_text=True)
-    app.logger.info("Request body: " + body)
+    if request.method == "POST":
+        payload = request.json
+        Reply_token = payload["event"][0]["replyToken"]
+        print(Reply_token)
+        message = payload["events"][0]["message"]["text"]
+        print(message)
+        if "how" in message:
+            Reply_message = "Good job"
+            ReplyMessage(Reply_token,Reply_message,"ziev+1/ECWJDjw1CkOPjOMofjQ5mft0H0XtZknC/Vu+KnGZzi+2vFVF34UiX+QOdh4JADi+j/xeyPeSiGjyhnvTvKjNijstiixgQeY77aBxJ7R0B8TS/BMCG/y8KheHMwAZ7TJFKN6i5UPBoRzm2BQdB04t89/1O/w1cDnyilFU=")
+        return request.json, 200
 
-    try:
-        handler.handle(body, signature)
-    except InvalidSignatureError:
-        abort(400)
-    return "OK"
-
-@handler.add(MessageEvent, message=TextMessage)
-def handle_message(event):
-    value = chatbot.get_response(MessageEvent)
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=value)
-    )
+def ReplyMessage(Reply_token, TextMessage, Line_Acees_Token):
+    LINE_API = "https://api.line.me/v2/bot/message/reply"
+    Authorization = "Bearer {}".format(Line_Acees_Token)
+    print(Authorization)
+    headers = {
+        "Content-Type" : "application/json; charset=UTF-8",
+        "Authorization":Authorization
+    }
+    data = {
+        "replyToken":Reply_token,
+        "messages":[{
+            "type":"text",
+            "text":TextMessage
+        }]
+    }
+    data = json.dumps(data)
+    r = request.post(LINE_API, headers=headers, data=data)
+    return 200
 
 
 if __name__ == "__main__":
