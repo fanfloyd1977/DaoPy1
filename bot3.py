0d9308c8aa8b07ddd75b840c9660794797330aab
fanfloyd1977 <77666154+fanfloyd1977@users.noreply.github.com>
2021-02-01 18:00:03 +0700

    Add files via upload

--- bot3.py
+++ bot3.py
@@ -133,12 +133,11 @@ def handle_message(event):
                     line_bot_api.reply_message(
                     event.reply_token,
                     TextSendMessage(text=reply_text))
-                 else:
-                        reply_text = "ok"
-                        line_bot_api.reply_message(
-                        event.reply_token,
-                        TextSendMessage(text=reply_text))
-
+            else:
+                reply_text = "ok"
+                line_bot_api.reply_message(
+                    event.reply_token,
+                    TextSendMessage(text=reply_text))
 
 
 if __name__ == "__main__":
