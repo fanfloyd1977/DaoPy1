1d3057b55b0244af11080f759f1b1f950e578578
fanfloyd1977 <77666154+fanfloyd1977@users.noreply.github.com>
2021-02-04 09:58:40 +0700

    Add files via upload

--- bot3.py
+++ bot3.py
@@ -158,13 +158,10 @@ def handle_message(event):
                     if res:
                         value = randrange(1, row)
                         reply_text = data2["Answer"].values[value]
-                        if len(data2["Answers"].values[value]) != 0:
-                            reply_text2 = data2["Answer2"].values[value]
-                            line_bot_api.reply_message(event.reply_token,[TextSendMessage(text=reply_text),TextSendMessage(text=reply_text2)])
-                        else:
-                            line_bot_api.reply_message(
-                            event.reply_token,
-                            TextSendMessage(text=reply_text))
+                        reply_text2 = data2["Answer2"].values[value]
+                        line_bot_api.reply_message(
+                        event.reply_token,
+                        [TextSendMessage(text=reply_text),TextSendMessage(text=reply_text2)])
 
 
 
