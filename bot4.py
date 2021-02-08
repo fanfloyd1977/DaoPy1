f5b220adbdb0e6c3d4929ab1635cde5aa28f34b2
fanfloyd1977 <77666154+fanfloyd1977@users.noreply.github.com>
2021-02-08 11:12:38 +0700

    Add files via upload

--- bot4.py
+++ bot4.py
@@ -1,57 +1,61 @@
+from pandas import pandas as pd
+
+from random import randrange
 from flask import Flask, request, abort
-import requests
-import json
 
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
-from linebot.models import MessageEvent, TextMessage, TextSendMessage
+from linebot.models import *
 
 app = Flask(__name__)
 
 line_bot_api = LineBotApi("ziev+1/ECWJDjw1CkOPjOMofjQ5mft0H0XtZknC/Vu+KnGZzi+2vFVF34UiX+QOdh4JADi+j/xeyPeSiGjyhnvTvKjNijstiixgQeY77aBxJ7R0B8TS/BMCG/y8KheHMwAZ7TJFKN6i5UPBoRzm2BQdB04t89/1O/w1cDnyilFU=")
 handler = WebhookHandler("4088552f2e9ee28de065d9bddce75ab2")
 
+@app.route("/")
+def hello():
+    return "Hello DAO Flask-Heroku"
 
-#@app.route("/")
-#def hello():
-#    return "Hello DAO Flask-Heroku"
 
+@app.route("/callback", methods=["POST"])
+def callback():
+    signature = request.headers["X-Line-Signature"]
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
 
-@app.route("/", methods=["POST", "GET"])
-def webhook():
-    if request.method == "POST":
-        payload = request.json
-        Reply_token = payload["event"][0]["replyToken"]
-        print(Reply_token)
-        message = payload["events"][0]["message"]["text"]
-        print(message)
-        if "how" in message:
-            Reply_message = "Good job"
-            ReplyMessage(Reply_token,Reply_message,"ziev+1/ECWJDjw1CkOPjOMofjQ5mft0H0XtZknC/Vu+KnGZzi+2vFVF34UiX+QOdh4JADi+j/xeyPeSiGjyhnvTvKjNijstiixgQeY77aBxJ7R0B8TS/BMCG/y8KheHMwAZ7TJFKN6i5UPBoRzm2BQdB04t89/1O/w1cDnyilFU=")
-        return request.json, 200
-    else:
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
         abort(400)
+    return "OK"
 
-def ReplyMessage(Reply_token, TextMessage, Line_Acees_Token):
-    LINE_API = "https://api.line.me/v2/bot/message/reply"
-    Authorization = "Bearer {}".format(Line_Acees_Token)
-    print(Authorization)
-    headers = {
-        "Content-Type" : "application/json; charset=UTF-8",
-        "Authorization":Authorization
-    }
-    data = {
-        "replyToken":Reply_token,
-        "messages":[{
-            "type":"text",
-            "text":TextMessage
-        }]
-    }
-    data = json.dumps(data)
-    r = request.post(LINE_API, headers=headers, data=data)
-    return 200
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
 
+#Image
+    if event.message.text.lower() == "send image":
+        image = pd.read_excel(r"Image.xls")
+        row = image.shape[0]
+        value = randrange(1, row)
+        reply_text = image["Url"].values[value]
+        line_bot_api.reply_message(
+            event.reply_token,
+            ImageSendMessage(original_content_url= reply_text, preview_image_url= reply_text))
+#Sticker
+    if event.message.text.lower() in "send me sticker" or event.message.text.lower() in ["^^", ":)", ";)", "\^o^/"]:
+        value = randrange(180, 259)
+        line_bot_api.reply_message(
+            event.reply_token,
+            StickerSendMessage(package_id=3, sticker_id=value))
 
-if __name__ == "__main__":
-    app.run()
-
+#Book
+    if True:
+        data = pd.read_excel(r"Book3.xls")
+        row = data.shape[0]
+        for i in range(row):
+            res = i in data["Question"].values[i].lower()
+            if res:
+                reply_text = data["Answer"].values[i]
+                line_bot_api.reply_message(
+                    event.reply_token,
+                    TextSendMessage(text=reply_text))
