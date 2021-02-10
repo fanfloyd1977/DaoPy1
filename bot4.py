00ea3f4061f0e4375a49e1a6842fe16d9ea33dbe
fanfloyd1977 <77666154+fanfloyd1977@users.noreply.github.com>
2021-02-10 21:36:17 +0700

    Add files via upload

--- bot4.py
+++ bot4.py
@@ -76,7 +76,8 @@ def handle_message(event):
         M = max(Col_data)
         A = sheet.cell(Col_data.index(M)+1,2).value
         line_bot_api.reply_message(event.reply_token,TextSendMessage(text=A))
-
+    
+@handler.add(MessageEvent, message=TextMessage)
 def job(event):
     line_bot_api.reply_message(event.reply_token,TextSendMessage(text="Good day 2 u"))
 
