038ee6022e0ae8a0c91f7180dc575ac37718f177
fanfloyd1977 <77666154+fanfloyd1977@users.noreply.github.com>
2021-02-11 09:38:51 +0700

    Add files via upload

--- bot4.py
+++ bot4.py
@@ -77,7 +77,7 @@ def handle_message(event):
         A = sheet.cell(Col_data.index(M)+1,2).value
         line_bot_api.reply_message(event.reply_token,TextSendMessage(text=A))
 
-
+@handler.add(FollowEvent)
 def job(event):
     line_bot_api.push_message((event.reply_token,TextSendMessage(text="Good Day Dao")))
     #schedule.every(10).seconds.do(job)
