77758c17ff6b61f4256f7e63d3ec1c7980ff7b8b
fanfloyd1977 <77666154+fanfloyd1977@users.noreply.github.com>
2021-02-11 15:43:55 +0700

    Add files via upload

--- bot4.py
+++ bot4.py
@@ -57,6 +57,7 @@ def handle_message(event):
         line_bot_api.reply_message(
             event.reply_token,
             StickerSendMessage(package_id=3, sticker_id=value))
+        value = randrange(180, 259)
         line_bot_api.reply_message(
             event.reply_token,
             StickerSendMessage(package_id=3, sticker_id=value))
