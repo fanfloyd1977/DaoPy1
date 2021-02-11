02f4d16d3e83454fa8a916d7c4e3aed3b3db0e0f
fanfloyd1977 <77666154+fanfloyd1977@users.noreply.github.com>
2021-02-11 13:39:08 +0700

    Add files via upload

--- bot4.py
+++ bot4.py
@@ -75,8 +75,8 @@ def handle_message(event):
         Col_data = sheet.col_values(3)
         M = max(Col_data)
         A = sheet.cell(Col_data.index(M)+1,2).value
-        line_bot_api.reply_message(event.reply_token,TextSendMessage(text=A))
-        line_bot_api.reply_message(event.reply_token,StickerSendMessage(package_id=3, sticker_id=180))
+        line_bot_api.reply_message(event.reply_token,[TextSendMessage(text=A),StickerSendMessage(package_id=3, sticker_id=180)])
+
 
 
 
