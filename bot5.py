05b1e66fa2f3d32780e799ea458a4db709696e01
fanfloyd1977 <77666154+fanfloyd1977@users.noreply.github.com>
2021-02-18 09:33:17 +0700

    Add files via upload

---
+++ bot5.py
@@ -0,0 +1,56 @@
+import time
+import schedule
+
+from pandas import pandas as pd
+
+from random import randrange
+from flask import Flask, request, abort
+
+from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError
+from linebot.models import *
+
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+
+
+app = Flask(__name__)
+
+#Bot key
+line_bot_api = LineBotApi("ziev+1/ECWJDjw1CkOPjOMofjQ5mft0H0XtZknC/Vu+KnGZzi+2vFVF34UiX+QOdh4JADi+j/xeyPeSiGjyhnvTvKjNijstiixgQeY77aBxJ7R0B8TS/BMCG/y8KheHMwAZ7TJFKN6i5UPBoRzm2BQdB04t89/1O/w1cDnyilFU=")
+handler = WebhookHandler("4088552f2e9ee28de065d9bddce75ab2")
+
+#Goole sheet Key
+
+scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
+creds = ServiceAccountCredentials.from_json_keyfile_name('fanfloyd1977-2bf294ca8a0e.json', scope)
+client = gspread.authorize(creds)
+
+#Intent
+greeting = ["Hello Hello","Hi Hi", "Hi", "Hi, there", "Good day", "สวัสดีครับ", "สวัสดีค่ะ"]
+
+@app.route("/")
+def hello():
+    return "Hello DAO Flask-Heroku"
+
+@app.route("/callback", methods=["POST"])
+def callback():
+    signature = request.headers["X-Line-Signature"]
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+    return "OK"
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    #Intent greeting
+    if event.message.text.lower() in [low.lower() for low in greeting]:
+        value = randrange(0, len(greeting))
+        reply_text = greeting[value]
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text=reply_text))
