dda6a4f87214c09f682e9464ec557f5d53ed63dd
fanfloyd1977 <77666154+fanfloyd1977@users.noreply.github.com>
2021-02-23 14:25:47 +0700

    Add files via upload

--- bot5.py
+++ bot5.py
@@ -138,9 +138,8 @@ def handle_message(event):
                     )
                 ]
         )
-
-
         )
+    line_bot_api.reply_message(event.reply_token,menu)
 
 
 @handler.add(PostbackEvent)
