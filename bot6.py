ad66b618c96dd585176edddc3889b03db39179a9
fanfloyd1977 <77666154+fanfloyd1977@users.noreply.github.com>
2021-03-05 09:56:32 +0700

    Add files via upload

--- bot6.py
+++ bot6.py
@@ -70,18 +70,7 @@ def handle_message(event):
         sum = 0
         num_row = sheet.col_values(1)
         row=len(num_row)
-        #for i in range(2,row+1):
-            #cus = sheet.row_values(i)
-            #if cus[7] == profile.user_id and cus[6] != "CHECKED":
-                #row_data = sheet.row_values(i)
-                #sheet.update_cell(i,7,"CHECKED")
-                #sum = sum + int(cus[3])
-                #bill_text = [TextSendMessage(text=row_data[1] +"   "+ row_data[2] +"   "+ row_data[3])]
-                #line_bot_api.push_message(profile.user_id, bill_text)
-
-        #line_bot_api.push_message(profile.user_id, TextSendMessage(text="TOTAL Bill = " + str(sum)))
-
-
+## Bill header
         Bill_bubble1 = BubbleContainer(
             direction='ltr',
             body=BoxComponent(
@@ -93,19 +82,17 @@ def handle_message(event):
                 TextComponent(text="Cafe Camellia",weight="bold",size="lg"),
                 TextComponent(text="Ladprao Street,3-9-2-8 Ladprao, Bangkok",weight="bold",size="xxs")
             ]))
-
         BB_message1 = FlexSendMessage(alt_text="Hello T_bubble", contents=Bill_bubble1)
         line_bot_api.push_message(profile.user_id,BB_message1)
-
+## Bill Body
         for j in range(2,row+1):
             cus = sheet.row_values(j)
             if cus[7] == profile.user_id and cus[6] != "CHECKED":
-                #row_data = sheet.row_values(i)
                 sheet.update_cell(j,7,"CHECKED")
                 sum = sum + int(cus[3])
                 bill_text = [TextSendMessage(text=cus[1] +"   "+ cus[2] +"         "+ cus[3]+ " BHT")]
                 line_bot_api.push_message(profile.user_id, bill_text)
-
+## Bill Footer
 
         Bill_bubble2 = BubbleContainer(
             direction='ltr',
