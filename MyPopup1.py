16285a7c95a5eead507cdcaa6a612d0dc1501575
fanfloyd1977 <77666154+fanfloyd1977@users.noreply.github.com>
2021-04-05 13:44:37 +0700

    Add files via upload

--- MyPopup1.py
+++ MyPopup1.py
@@ -1,17 +1,15 @@
 from tkinter import *
 from gtts import gTTS
 import os
+from flask import Flask, request, abort
+
+app = Flask(__name__)
+
 
 @app.route("/")
 def hello():
     return "Hello My first smart pop-up"
-    popup = tk.Tk()
-    popup.wm_title("!")
-    label = ttk.Label(popup, text=msg, font=NORM_FONT)
-    label.pack(side="top", fill="x", pady=10)
-    B1 = ttk.Button(popup, text="Okay", command = popup.destroy)
-    B1.pack()
-    popup.mainloop()
+
 
 
 
