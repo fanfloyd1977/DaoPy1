a95d3aca31c7c3c50b1a292f00a8ee4c0d2f6902
fanfloyd1977 <77666154+fanfloyd1977@users.noreply.github.com>
2021-11-18 11:01:28 +0700

    Update Skingenics.py

--- Skingenics.py
+++ Skingenics.py
@@ -199,7 +199,7 @@ def handle_postback(event):
         sheet = client.open("Booktwo").sheet1
         row = sheet.col_values(1)
         datetime = (datetime.fromtimestamp(event.timestamp) - timedelta(hours=2)).strftime('%Y-%m-%d %H:%M:%S')
-        Gris_Regular_Row = [len(row),"Gris","Regular",2500,1,profile.display_name,"New",profile.user_id,datetime]
+        Gris_Regular_Row = [len(row),"Gris","Regular",2500,1,profile.display_name,"New",profile.user_id,event.timestamp]
         sheet.append_row(Gris_Regular_Row)
 
     if data == "Gris Large":
